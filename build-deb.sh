f0ce41de832d0173257cbfa45647e75d73231cfb
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-06-27 15:33:57 +0900

    Create build-deb.sh

---
+++ build-deb.sh
@@ -0,0 +1,17 @@
+cd deb
+cd Roboto_Mono
+mkdir usr
+cd usr
+mkdir share
+cd share
+wget -O roboto-mono.zip https://fonts.google.com/download?family=Roboto+Mono
+mkdir Roboto_Mono
+unzip roboto-mono.zip -d Roboto_Mono
+mv ./Roboto_Mono/static/*.ttf ./
+rm -r Roboto_Mono
+mkdir Roboto_Mono
+mv ./*.ttf Roboto_Mono
+rm roboto-mono.zip
+cd ../../..
+dpkg -b Roboto_Mono
+cd ..
