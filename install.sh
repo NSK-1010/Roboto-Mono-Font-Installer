a1bcc9af1eb1c42b31d514ec9bd2f43c9262c411
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-07-04 20:26:24 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,38 @@
-#!/bin/bash
+require 'open-uri'
+@path = 'https://fonts.google.com/download?family=Roboto+Mono'
 
-wget -O roboto-mono.zip https://fonts.google.com/download?family=Roboto+Mono
-mkdir Roboto_Mono
-unzip roboto-mono.zip -d Roboto_Mono
-sudo mkdir /usr/share/fonts/Roboto_Mono
-sudo mv ./static/*.ttf /usr/share/fonts/Roboto_Mono/
-rm -r Roboto_Mono
-rm roboto-mono.zip
+fileName = "roboto-mono.zip"
+
+open(fileName, 'rwa+b') do |output|
+  open(@path) do |data|
+    output.write(data.read)
+  end
+end
+
+require "fileutils"
+FileUtils.mkdir("Roboto_Mono")
+
+require 'zip'
+
+dest = 'Roboto_Mono'
+
+Zip::File.open('roboto-mono.zip') do |zip|
+  zip.each do |entry|
+    p entry.name
+    zip.extract(entry, dest + entry.name) { true }
+  end
+end
+
+require "fileutils"
+FileUtils.mkdir("/usr/share/fonts/Roboto_Mono")
+
+require "fileutils"
+
+Dir.glob('*.ttf').each do |fontname|
+FileUtils.mv("Roboto_Mono/static/" + fontname, "/usr/share/fonts/Roboto_Mono/" + fontname)
+
+reqiure 'fileutils'
+FileUtils.rm_rf("Roboto_Mono")
+
+require 'fileutils'
+FileUtils.rm("roboto-mono.zip")
